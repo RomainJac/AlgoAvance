1c3a1fba72c08b9cee367be3bcdfe99be052505a
Romain <romainjacob012@gmail.com>
2024-02-29 12:12:33 +0100

    Fix

--- src/main/java/fr/pantheonsorbonne/cri/App.java
+++ src/main/java/fr/pantheonsorbonne/cri/App.java
@@ -1,29 +1,26 @@
 package fr.pantheonsorbonne.cri;
 
-import java.util.Random;
-
 public class App {
     public static void main(String[] args) {
-            addElements(0);
-            addElements(10);
-            addElements(100);
-            addElements(1000);
-            addElements(100000);
-            addElements(10000000);
-        }
-    
+        addElements(0);
+        addElements(10);
+        addElements(100);
+        addElements(1000);
+        addElements(100000);
+        addElements(10000000);
+    }
+
     public static void addElements(int numElements) {
         ArrayListe list = new ArrayListe();
-    
+
         long startTime = System.nanoTime();
         for (int i = 0; i < numElements; i++) {
             list.add("Element " + i);
         }
         long endTime = System.nanoTime();
 
-        long duration = (endTime - startTime) / 1000000; // Convert nanoseconds to milliseconds
+        long duration = (endTime - startTime) / 1000000; 
         System.out.println("Ajout de " + numElements + " elements en " + duration + " milliseconds");
     }
 
 }
-
