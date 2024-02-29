7d7ed9e4f35cd11021287329a1c99dd6ed3bb20d
Romain <romainjacob012@gmail.com>
2024-02-29 12:05:30 +0100

    Fix

--- src/main/java/fr/pantheonsorbonne/cri/App.java
+++ src/main/java/fr/pantheonsorbonne/cri/App.java
@@ -1,66 +1,37 @@
-import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartPanel;
-import org.jfree.chart.JFreeChart;
-import org.jfree.data.xy.DefaultXYDataset;
+package fr.pantheonsorbonne.cri;
 
-import javax.swing.*;
-import java.awt.*;
+import java.util.Random;
 
-public class ChartApp extends JFrame {
-    private DefaultXYDataset dataset;
-
-    public ChartApp(String title) {
-        super(title);
-
-        dataset = new DefaultXYDataset();
-        JFreeChart chart = ChartFactory.createXYLineChart(
-                "Temps d'exécution de la méthode contains",
-                "Taille de la liste",
-                "Temps d'exécution (ms)",
-                dataset
-        );
-
-        ChartPanel chartPanel = new ChartPanel(chart);
-        chartPanel.setPreferredSize(new Dimension(800, 600));
-        getContentPane().add(chartPanel);
-    }
-
-    public void addData(double[][] data, String seriesName) {
-        dataset.addSeries(seriesName, data);
-    }
-
+public class App {
     public static void main(String[] args) {
-        ChartApp chartApp = new ChartApp("Performance test");
-        chartApp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        // Ajoutez les données de performance
-        chartApp.addData(createData(1000), "1000");
-        chartApp.addData(createData(10000), "10000");
-        chartApp.addData(createData(100000), "100000");
-        chartApp.addData(createData(1000000), "1000000");
-
-        chartApp.pack();
-        chartApp.setVisible(true);
+        testContains(1000);
+        testContains(10000);
+        testContains(100000);
+        testContains(1000000);
+        testContains(10000000);
     }
 
-    private static double[][] createData(int size) {
+    public static void testContains(int size) {
         ArrayListe list = new ArrayListe();
         Random random = new Random();
-        double[][] data = new double[2][size];
 
-        for (int i = 0; i < size; i++) {
-            list.add("Element_" + random.nextInt());
+        // Remplissage de la liste avec des éléments aléatoires
+        for (long i = 0; i < size; i++) {
+            list.add("Element_" + random.nextInt()); // Utilisation de nextLong() sans argument pour obtenir un long aléatoire
         }
 
+        // Recherche d'un élément aléatoire dans la liste
+        String randomElement = "Element_" + random.nextInt();
+
+        // Test de la méthode contains
         long startTime = System.currentTimeMillis();
-        for (int i = 0; i < size; i++) {
-            list.contains("Element_" + random.nextInt());
-        }
+        boolean contains = list.contains(randomElement);
         long endTime = System.currentTimeMillis();
 
-        data[0][0] = size;
-        data[1][0] = endTime - startTime;
-
-        return data;
+        // Affichage des résultats
+        System.out.println("Taille de la liste : " + size);
+        System.out.println("La liste contient l'élément '" + randomElement + "' ? " + contains);
+        System.out.println("Temps d'exécution pour contains : " + (endTime - startTime) + " ms");
+        System.out.println();
     }
 }
