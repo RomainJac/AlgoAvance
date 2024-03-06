c17228a910e32a7389896c6f481597967d21b216
Lucian Lisnic <lisniclucian@gmail.com>
2024-03-06 15:14:50 +0100

    finished stack with linkedlist type impl. aswell as fully fledged unit testing

--- src/main/java/fr/pantheonsorbonne/cri/StackLL.java
+++ src/main/java/fr/pantheonsorbonne/cri/StackLL.java
@@ -1,28 +1,33 @@
 package fr.pantheonsorbonne.cri;
 
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.NoSuchElementException;
 
-public class StackLL<E> {
+public class StackLL<E> implements Iterable<E> {
 
     private ElementStackLL<E> last;
+
     public StackLL() {
         this.last = null;
     }
+
     public StackLL(E e) {
         ElementStackLL<E> element = new ElementStackLL<>();
         element.setValue(e);
         this.last = element;
     }
+
     public void push(E e) {
         if (this.isEmpty()) {
             this.last = new ElementStackLL<>(e);
-        }
-        else {
+        } else {
             ElementStackLL<E> element = new ElementStackLL<>(e);
             element.setPrevious(this.last);
             this.last = element;
         }
     }
+
     public E pop() {
         if (this.isEmpty()) {
             return null;
@@ -31,14 +36,37 @@ public class StackLL<E> {
         last = last.getPrevious();
         return value;
     }
+
     public E peek() {
         if (this.isEmpty()) {
             return null;
         }
         return last.getValue();
     }
+
     public boolean isEmpty() {
         return last == null;
     }
-}
 
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            ElementStackLL<E> current = last;
+
+            @Override
+            public boolean hasNext() {
+                return current != null;
+            }
+
+            @Override
+            public E next() {
+                if (current == null) {
+                    throw new NoSuchElementException();
+                }
+                E value = current.getValue();
+                current = current.getPrevious();
+                return value;
+            }
+        };
+    }
+}
--- src/main/java/fr/pantheonsorbonne/cri/TestPerformance.java
+++ src/main/java/fr/pantheonsorbonne/cri/TestPerformance.java
@@ -1,23 +1,11 @@
 package fr.pantheonsorbonne.cri;
 
-import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
-
-import java.awt.*;
-import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartPanel;
-import org.jfree.chart.JFreeChart;
-import org.jfree.data.category.DefaultCategoryDataset;
-import org.jfree.data.xy.XYSeries;
-import org.jfree.data.xy.XYSeriesCollection;
-
 /*
  * Les méthodes utilisent Random, sauf si on lui passe true en argument, dans ce cas là on
  * cherche le dernier élément ajouté.
  * 
  */
 public class TestPerformance {
-
     public double hashSetTime(int nbElem, boolean deterministe) {
         StringHashSet set = new StringHashSet();
         for (int i = 0; i < nbElem; i++) {
@@ -31,7 +19,7 @@ public class TestPerformance {
             set.contains("elem" + rand);
         }
         long end = System.nanoTime();
-        return (end - start);
+        return end - start;
     }
 
     public double linkedListeTime(int nbElem, boolean deterministe) {
@@ -47,7 +35,8 @@ public class TestPerformance {
             list.contains("elem" + rand);
         }
         long end = System.nanoTime();
-        return (end - start);
+        return end - start;
+
     }
 
     public double treeSetTime(int nbElem, boolean deterministe) {
@@ -66,113 +55,19 @@ public class TestPerformance {
         return end - start;
     }
 
-    public double averageTime(int nbElem) {
-        double totalTime = 0;
-
-        for (int i = 0; i < 20; i++) {
-            totalTime += linkedListeTime(nbElem, false);
-        }
-
-        return totalTime / 20;
-    }
-
-    public double averageTimeHashSet(int nbElem) {
-        double totalTime = 0;
-
-        for (int i = 0; i < 20; i++) {
-            totalTime += hashSetTime(nbElem, false);
-        }
-
-        return totalTime / 20;
-    }
-
-    public double averageTimeTreeSet(int nbElem) {
-        double totalTime = 0;
-
-        for (int i = 0; i < 20; i++) {
-            totalTime += treeSetTime(nbElem, false);
-        }
-
-        return totalTime / 20;
-    }
-
-    public void runTestsGraphique() {
-        DefaultCategoryDataset barChartDataset = new DefaultCategoryDataset();
-        XYSeries lineChartLinkedListSeries = new XYSeries("LinkedList");
-        XYSeries lineChartHashSetSeries = new XYSeries("HashSet");
-        XYSeries lineChartTreeSetSeries = new XYSeries("TreeSet");
-
-        int[] sizes = { 100, 500, 1000, 10000, 25_000, 50_000, 100000, 150_000, 200_000, 250_000, 300_000, 350_000,
-                400_000, 450_000, 500_000 };
-        for (int size : sizes) {
-            double averageLinkedListTime = averageTime(size);
-            double averageHashSetTime = averageTimeHashSet(size);
-            double averageTreeSetTime = averageTimeTreeSet(size);
-            barChartDataset.addValue(averageLinkedListTime, "LinkedList", "Size: " + size);
-            barChartDataset.addValue(averageHashSetTime, "HashSet", "Size: " + size);
-            barChartDataset.addValue(averageTreeSetTime, "TreeSet", "Size: " + size);
-            lineChartLinkedListSeries.add(size, averageLinkedListTime);
-            lineChartHashSetSeries.add(size, averageHashSetTime);
-            lineChartTreeSetSeries.add(size, averageTreeSetTime);
-        }
-
-        JFreeChart barChart = ChartFactory.createBarChart(
-                "Comparaison des performances (contains)",
-                "Taille de la collection",
-                "Temps d'exécution moyen (en ns)",
-                barChartDataset);
-
-        XYSeriesCollection lineChartDataset = new XYSeriesCollection();
-        lineChartDataset.addSeries(lineChartLinkedListSeries);
-        lineChartDataset.addSeries(lineChartHashSetSeries);
-        lineChartDataset.addSeries(lineChartTreeSetSeries);
-
-        JFreeChart lineChart = ChartFactory.createXYLineChart(
-                "Comparaison des performances (contains)",
-                "Taille de la collection",
-                "Temps d'exécution moyen (en ns)",
-                lineChartDataset);
-
-        ChartPanel barChartPanel = new ChartPanel(barChart);
-        ChartPanel lineChartPanel = new ChartPanel(lineChart);
-
-        JFrame frame = new JFrame("Comparaison des performances (contains)");
-        frame.setLayout(new GridLayout(2, 1));
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(barChartPanel);
-        frame.add(lineChartPanel);
-        frame.pack();
-        frame.setVisible(true);
-    }
-
-    public void runTestsTableau() {
-        int[] sizes = { 100, 1000, 10000, 100000 };
-        Object[][] data = new Object[sizes.length][4];
-
-        for (int i = 0; i < sizes.length; i++) {
-            int size = sizes[i];
-            data[i][0] = size;
-            data[i][1] = averageTime(size);
-            data[i][2] = averageTimeHashSet(size);
-            data[i][3] = averageTimeTreeSet(size);
-        }
-
-        String[] columnNames = { "Taille de la collection", "LinkedList", "HashSet", "TreeSet" };
-
-        DefaultTableModel model = new DefaultTableModel(data, columnNames);
-        JTable table = new JTable(model);
-        JScrollPane scrollPane = new JScrollPane(table);
-
-        JFrame frame = new JFrame("Tableau de comparaison des performances (contains)");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
-        frame.pack();
-        frame.setVisible(true);
-    }
-
     public static void main(String... args) {
+        //En nanosecondes
         TestPerformance test = new TestPerformance();
-        test.runTestsGraphique();
-        test.runTestsTableau();
+        System.out.println("hashset: " + test.hashSetTime(1000000, true));
+        System.out.println("hashet: " + test.hashSetTime(100000, true));
+        System.out.println("hashet: " + test.hashSetTime(10000, true));
+        System.out.println("linkedListe: " + test.linkedListeTime(1000000, true));
+        System.out.println("linkedListe: " + test.linkedListeTime(100000, true));
+        System.out.println("linkedListe: " + test.linkedListeTime(10000, true));
+        System.out.println("treeSet: " + test.treeSetTime(1000000, true));
+        System.out.println("treeSet: " + test.treeSetTime(100000, true));
+        System.out.println("treeSet: " + test.treeSetTime(10000, true));
     }
+
+
 }
